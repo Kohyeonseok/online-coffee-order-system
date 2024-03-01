c888e3798b018c8716866ba22b79aa91f34938d0
Kohyeonseok <kohunsuk@gmail.com>
2024-03-01 18:29:02 +0900

    create customer system

---
+++ src/main/java/com/example/demo/ordersystem/domain/CreateOrder.java
@@ -0,0 +1,15 @@
+package com.example.demo.ordersystem.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class CreateOrder {
+    private int customerId;
+    private Map<Integer,Integer> quantityByProduct; //["아이스 아메리카노",3]
+
+
+}
---
+++ src/main/java/com/example/demo/ordersystem/domain/StoreProduct.java
@@ -0,0 +1,20 @@
+package com.example.demo.ordersystem.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name="store_Products")
+public class StoreProduct {
+    @Id
+    private String storeProductId;
+
+    @Column
+    private String storeId;
+
+    @Column
+    private String productId;
+
+    @Column
+    private String stockQuantity;
+}
